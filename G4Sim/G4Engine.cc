2dbcabba53efd5ee41963f8aeacade340db8d672
Jake <calcuttj@msu.edu>
2018-07-06 17:08:15 -0500

    Updated Engine. Can access process manager, active processes, can make dummy step,steppoint,track. Can associate them and add material to the step point

--- G4Sim/G4Engine.cc
+++ G4Sim/G4Engine.cc
@@ -4,6 +4,10 @@
 #include "G4SimActionInitialization.hh"
 #include "G4ParticleTable.hh"
 #include "G4ParticleDefinition.hh"
+#include "G4ProcessManager.hh"
+#include "G4Step.hh"
+#include "G4StepPoint.hh"
+#include "G4Track.hh"
 
 int main(int argc, char ** argv){
   G4RunManager * rm = new G4RunManager;
@@ -19,6 +23,29 @@ int main(int argc, char ** argv){
 
   G4ParticleDefinition * piplus = pt->FindParticle("pi+");
   std::cout << piplus->GetPDGMass() << std::endl;
+
+  G4ProcessManager * pm = piplus->GetProcessManager();
+  auto pv = pm->GetProcessList();
+
+  G4Step * step = new G4Step();
+  G4Track * track = new G4Track();
+  G4StepPoint * point = new G4StepPoint();
+
+  step->SetPreStepPoint(point);
+  track->SetStep(step);
+  step->SetTrack(track);
+
+//  G4SimDetectorConstruction * det = new G4SimDetectorConstruction();
+//  auto con = det->Construct();
+  G4Material * water = nist->FindOrBuildMaterial("G4_WATER");
+  point->SetMaterial(water);
+
+  std::cout << pv->size() << std::endl;
+
+  for(int i = 0; i < pv->entries(); ++i){
+    std::cout << (*pv)(i)->GetProcessName() << std::endl;
+  }
+
 
   return 0.;
 }

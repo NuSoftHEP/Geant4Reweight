d6838fd235c62fef30cea650deb1598610c2fbab
Jake <calcuttj@msu.edu>
2019-02-26 19:16:42 -0600

    Important: Added a FitParameter struct that should have been added in the last commit.

---
+++ FitterBase/include/FitParameter.hh
@@ -0,0 +1,13 @@
+#ifndef FitParameter_h
+#define FitParameter_h 1
+
+#include <string>
+
+struct FitParameter{
+  std::string Name;
+  std::string Cut;
+  double Value;
+  bool Dummy;
+  std::pair< double, double > Range;
+};
+#endif

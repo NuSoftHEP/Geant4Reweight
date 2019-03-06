8c2aba4fb2ea31dddfa2949d8a4ae0012f31625a
Jake <calcuttj@msu.edu>
2019-03-05 18:06:56 -0600

    Fleshed out script to find minimum

--- FitterBase/FindMinium.py
+++ FitterBase/FindMinium.py
@@ -17,12 +17,27 @@ par_names = [ i.GetName() for i in thisdir.GetListOfKeys() ]
 print par_names
 
 tree = f.Get( "FitTree" )
+Chi2_br = array( "d", [0] )
+tree.SetBranchAddress( "Chi2", Chi2_br )
 pars = dict()
+par_vals = dict()
 for par_name in par_names:
   pars[ par_name ] = array( "d", [0] )
   tree.SetBranchAddress( par_name, pars[ par_name ] )
+  par_vals[ par_name ] = []
 
+chi2 = []
 for e in tree:
+  chi2.append( Chi2_br[0] )
   for name,val in pars.iteritems():
     print name, val[0]
-    
+    par_vals[ name ].append( val[0] )
+
+print chi2
+print par_vals
+
+min_index = chi2.index( min(chi2) )
+print "Minimum Chi2:", min(chi2)
+print "Point:"
+for name,vals in par_vals.iteritems():
+  print name, vals[ min_index ]

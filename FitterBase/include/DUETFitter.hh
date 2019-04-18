acea2594b2d5b260159e78ca04e5cb8f7dcf83df
Jake <calcuttj@msu.edu>
2019-04-18 10:30:20 -0500

    Forgot to add DUETFitter to replace newDUETFitter in the last commit

---
+++ FitterBase/include/DUETFitter.hh
@@ -0,0 +1,36 @@
+#ifndef DUETFitter_h
+#define DUETFitter_h
+
+#include "G4ReweightFitter.hh"
+#include "FitSample.hh"
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "TH1D.h"
+#include "TH2D.h"
+#include "TMatrixD.h"
+#include "TGraphErrors.h"
+#include "TGraph.h"
+#include "TFile.h"
+#include "TTree.h"
+
+#include "G4ReweightTreeParser.hh"
+
+class DUETFitter : public G4ReweightFitter { 
+  public:
+  
+    DUETFitter( TFile * output, std::string data_file );
+    virtual void   LoadData();
+    void SaveData( TDirectory * ) override;
+    double DoFit(bool fSave=true) override;
+
+
+  private:
+    TMatrixD * DUET_cov_matrix;
+    TMatrixD * DUET_cov_inv;
+    
+};
+
+#endif

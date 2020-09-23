0259f0c7357fb332167fc7196a1ac6f53a398b73
Jacob Calcutt <calcuttj@msu.edu>
2020-09-23 12:10:06 -0500

    Made some plotting scripts py3 compatible

--- geant4reweight/scripts/PredictionBase/draw_exclusive.py
+++ geant4reweight/scripts/PredictionBase/draw_exclusive.py
@@ -1,8 +1,8 @@
-from ROOT import * 
+import ROOT as RT
 import sys
-f = TFile( sys.argv[1] )
+f = RT.TFile( sys.argv[1] )
 
-gROOT.SetBatch(1)
+RT.gROOT.SetBatch(1)
 grabs = f.Get( "abs_" + sys.argv[2] )
 grcex = f.Get( "cex_" + sys.argv[2] )
 grinel = f.Get( "inel_" + sys.argv[2] )
@@ -17,14 +17,16 @@ elif( sys.argv[2] == "KE" ):
   title = "Kinetic Energy (MeV)"
 
 
-grtotal.SetMaximum(500.)
+#grtotal.SetMaximum(500.)
+ys = [grtotal.GetY()[i] for i in range(0, grtotal.GetN())]
+grtotal.SetMaximum(1.2*max(ys))
 grtotal.SetMinimum(0.)
 grtotal.GetXaxis().SetRangeUser( 0., [grabs.GetX()[i] for i in range(0, grabs.GetN())][-1] )
 grtotal.SetTitle(";Pion " + title + ";#sigma (mb)")
 grtotal.GetXaxis().SetTitleSize(.05)
 grtotal.GetYaxis().SetTitleSize(.05)
 grtotal.GetXaxis().SetTitleOffset(.9)
-grtotal.GetYaxis().SetTitleOffset(.9)
+grtotal.GetYaxis().SetTitleOffset(1.125)
 grtotal.GetXaxis().SetLabelSize(.04)
 grtotal.GetYaxis().SetLabelSize(.04)
 
@@ -36,18 +38,18 @@ grcex.SetLineColor(4)
 grdcex.SetLineColor(8)
 grprod.SetLineColor(46)
 
-c1 = TCanvas("c1","c1",500,400)
+c1 = RT.TCanvas("c1","c1",500,400)
 c1.SetTickx(1)
 c1.SetTicky(1)
-gStyle.SetOptStat(0)
-gPad.SetLeftMargin( gPad.GetLeftMargin() * 1.10 )
+RT.gStyle.SetOptStat(0)
+RT.gPad.SetLeftMargin( RT.gPad.GetLeftMargin() * 1.20 )
 
-grtotal.Draw()
-grabs.Draw("same")
-grcex.Draw("same")
-grinel.Draw("same")
-grdcex.Draw("same")
-grprod.Draw("same")
+grtotal.Draw("AC")
+grabs.Draw("same C")
+grcex.Draw("same C")
+grinel.Draw("same C")
+grdcex.Draw("same C")
+grprod.Draw("same C")
 
 #leg = TLegend(.6,.6,.85,.85)
 #leg.AddEntry( grtotal, "Total", "lp")
@@ -59,4 +61,5 @@ grprod.Draw("same")
 #
 #leg.Draw("same")
 
+RT.gPad.RedrawAxis()
 c1.SaveAs( sys.argv[3] + ".pdf")

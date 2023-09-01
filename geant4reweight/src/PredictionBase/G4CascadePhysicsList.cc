91a10f08ef4aac7fbc1bbc0610ce20f92e792d82
Jacob Calcutt <calcuttj@msu.edu>
2023-09-01 09:54:39 -0500

    cherry pick 515d111e

--- geant4reweight/src/PredictionBase/G4CascadePhysicsList.cc
+++ geant4reweight/src/PredictionBase/G4CascadePhysicsList.cc
@@ -9,6 +9,8 @@
 #include "Geant4/G4HadronElasticPhysics.hh"
 #include "Geant4/G4HadronElasticPhysicsHP.hh"
 
+#include "Geant4/G4PhysicsConstructorRegistry.hh"
+
 G4CascadePhysicsList::G4CascadePhysicsList(int list) : G4VModularPhysicsList(){
   SetVerboseLevel(0);
 
@@ -38,6 +40,58 @@ G4CascadePhysicsList::G4CascadePhysicsList(int list) : G4VModularPhysicsList(){
       break;
     }
   }
+}
+
+G4CascadePhysicsList::G4CascadePhysicsList(fhicl::ParameterSet pars)
+    : G4VModularPhysicsList() {
+  SetVerboseLevel(0);
+
+  RegisterPhysics(new G4DecayPhysics);
+  RegisterPhysics(new G4RadioactiveDecayPhysics);
+
+  auto em_phys = pars.get<std::string>("EMPhysics", ""); //TODO -- add default
+  /*if (!G4PhysicsConstructorRegistry::Instance()->IsKnownPhysicsConstructor(
+   *        em_phys)) {
+    //TODO -- throw an exception?
+  }*/
+  RegisterPhysics(
+      G4PhysicsConstructorRegistry::Instance()->GetPhysicsConstructor(
+          em_phys));
+  //RegisterPhysics(new G4EmStandardPhysics);
+
+  auto inelastic_phys = pars.get<std::string>("InelasticPhysics", "");//TODO -- add default
+  //TODO -- check
+  RegisterPhysics(
+      G4PhysicsConstructorRegistry::Instance()->GetPhysicsConstructor(
+          inelastic_phys));
+
+  auto elastic_phys = pars.get<std::string>("ElasticPhysics", "");//TODO -- add default
+  RegisterPhysics(
+      G4PhysicsConstructorRegistry::Instance()->GetPhysicsConstructor(
+          elastic_phys));
+
+  /*switch (list) {
+    case 0: {
+      RegisterPhysics(new G4HadronPhysicsQGSP_BERT());
+      RegisterPhysics(new G4HadronElasticPhysics());
+      break;
+    }
+    case 1: {
+      RegisterPhysics(new G4HadronPhysicsFTFP_BERT());
+      RegisterPhysics(new G4HadronElasticPhysics());
+      break;
+    }
+    case 2: {
+      RegisterPhysics(new G4HadronPhysicsQGSP_BERT_HP());
+      RegisterPhysics(new G4HadronElasticPhysicsHP());
+      break;
+    }
+    default: {
+      RegisterPhysics(new G4HadronPhysicsQGSP_BERT());
+      RegisterPhysics(new G4HadronElasticPhysics());
+      break;
+    }
+  }*/
 }
 
 G4CascadePhysicsList::~G4CascadePhysicsList(){}
--- geant4reweight/src/PredictionBase/G4CascadePhysicsList.hh
+++ geant4reweight/src/PredictionBase/G4CascadePhysicsList.hh
@@ -3,12 +3,14 @@
 
 #include "Geant4/G4VUserPhysicsList.hh"
 #include "Geant4/G4VModularPhysicsList.hh"
+#include "fhiclcpp/ParameterSet.h"
 
 class G4CascadePhysicsList : public G4VModularPhysicsList
 {
 
   public:
     G4CascadePhysicsList(int list = 0);
+    G4CascadePhysicsList(fhicl::ParameterSet pars);
     virtual ~G4CascadePhysicsList();
 
   protected:
